80e15eb323046cd04fce7d73f888ce850e7c7d96
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-22 04:53:44 +0100

    makeCss function

---
+++ src/functions.js
@@ -0,0 +1,13 @@
+function makeCss(rules){
+        //{color:red;background-color:white} => 'color:red ; background-color:white}'
+    let cssText = "";
+    for(let rule of rules){
+        cssText += rule + ":" + rules[rule] + ";"
+    }
+    return cssText
+}
+
+
+const functions = {makeCss()}
+
+export default functions;
