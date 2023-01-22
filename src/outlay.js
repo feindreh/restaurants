87c070065ebc8dbed7fef9f341d62cd91b5fcd24
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-22 09:34:30 +0100

    layout changes

--- src/outlay.js
+++ src/outlay.js
@@ -3,16 +3,13 @@ import gridMain from "./gridMain";
 const header = document.createElement("div");
 header.setAttribute("id", "header");
 
-const hero = document.createElement("div");
-hero.setAttribute("id", "hero");
-
 const content = document.createElement("div");
 content.setAttribute("id", "content");
 
 const footer = document.createElement("div");
 footer.setAttribute("id", "footer");
 
-gridMain.append(header, hero, content, footer);
+gridMain.append(header, content, footer);
 
 const outlay = gridMain;
 
