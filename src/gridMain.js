5fb4ce6a4f9851bdbdc6f4378a24c9b733d6703b
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-22 05:09:57 +0100

    console.log deletet

--- src/gridMain.js
+++ src/gridMain.js
@@ -3,9 +3,7 @@ import {makeCss} from "./functions";
 const rules = {
   display: 'grid',
   'grid-template': '200px 500px 500px 200px / 1fr',
-}
-
-console.log(rules)
+};
 
 const gridMain = document.createElement('div');
 gridMain.setAttribute('id','gridMain');
