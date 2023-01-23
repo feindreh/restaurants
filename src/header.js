b7691b140071ba7f2fb6a50efc706174aab49535
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-23 01:28:59 +0100

    homeFunction to generate home

--- src/header.js
+++ src/header.js
@@ -1,3 +1,6 @@
+import content from "./content";
+import homeFunction from "./home";
+
 const header = document.createElement("div");
 header.setAttribute("id", "header");
 
@@ -5,6 +8,10 @@ const home = document.createElement("button");
 home.setAttribute("type", "button");
 home.setAttribute("class","button");
 home.innerText = "Home";
+home.addEventListener("click",()=>{
+	content.removeChild(content.firstChild);
+	content.append(homeFunction());
+});
 
 const menu = document.createElement("button");
 menu.setAttribute("type", "button");
