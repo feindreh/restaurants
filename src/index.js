6c0e2989e73d192c517e4bef0fa29afb3297e610
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-23 01:42:01 +0100

    display something on load

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./style.css";
 import outlay from "./outlay/outlay.js";
+import homeFunction from "./functions/homeFunction";
+
 
 document.querySelector("#container").append(outlay);
+content.append(homeFunction());
