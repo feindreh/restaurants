106fd32fb85ba7b4f6dc33db295448029848f23b
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-23 01:05:47 +0100

    add header buttons

--- src/header.js
+++ src/header.js
@@ -1,4 +1,23 @@
 const header = document.createElement("div");
 header.setAttribute("id", "header");
 
+const home = document.createElement("button");
+home.setAttribute("type", "button");
+home.setAttribute("class","button");
+home.innerText = "Home";
+
+const menu = document.createElement("button");
+menu.setAttribute("type", "button");
+menu.setAttribute("class","button");
+menu.innerText = "Menu";
+
+const contact = document.createElement("button");
+contact.setAttribute("type", "button");
+contact.setAttribute("class","button");
+contact.innerText = "Contact";
+
+
+
+header.append(home,menu,contact);
+
 export default header;
