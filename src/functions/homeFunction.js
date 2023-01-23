269f92d411519a5557cd1f91a8ecba0bee820174
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-23 03:20:35 +0100

    home content layout

--- src/functions/homeFunction.js
+++ src/functions/homeFunction.js
@@ -1,6 +1,28 @@
+import makeContentDiv from "./makeContentDiv";
+
 export default function homeFunction(){
 	const home = document.createElement("div");
 	home.setAttribute("class","generated");
-	home.innerText = "home";
+	
+	
+
+	const header = makeContentDiv();
+	header.innerText = "Ich bin die Überschrift";
+
+	const introduction = makeContentDiv();
+	introduction.innerText ="Ich bin die Einführung";
+
+	const workingHours = makeContentDiv();
+	workingHours.innerText = "Ich bin die Öffnungszeit";
+
+	const location = makeContentDiv();
+	location.innerText = "Ich bin der Ort an dem du mich findest";
+
+
+
+
+
+	home.append(header,introduction,workingHours,location);
+
 	return home;
 }
