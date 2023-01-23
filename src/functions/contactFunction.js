23b01eba9300e6a585edda15970e440deadb1aee
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-23 03:40:11 +0100

    display content for contac and menu

--- src/functions/contactFunction.js
+++ src/functions/contactFunction.js
@@ -1,6 +1,22 @@
+import makeContentDiv from "./makeContentDiv";
+
 export default function contactFunction(){
 	const contact = document.createElement("div");
 	contact.setAttribute("class","generated");
-	contact.innerText = "contact";
+
+	const header = makeContentDiv();
+	header.innerText = "Ich bin die Überschrift";
+
+	const bagel1 = makeContentDiv();
+	bagel1.innerText = "I am Mr Best Bagel.";
+
+	const bagel2 = makeContentDiv();
+	bagel2.innerText = "I am Mrs Best Bagel.";
+
+	const bagel3 = makeContentDiv();
+	bagel3.innerText = "Just call me Bagly. \n Mr Best Bagel is my Father.";
+
+
+	contact.append(header,bagel1,bagel2,bagel3);
 	return contact;
 }
--- src/functions/menuFunction.js
+++ src/functions/menuFunction.js
@@ -1,6 +1,17 @@
+import makeContentDiv from "./makeContentDiv";
+
 export default function menuFunction(){
 	const menu = document.createElement("div");
 	menu.setAttribute("class","generated");
-	menu.innerText = "menu";
+
+	const header = makeContentDiv();
+	header.innerText = "Ich bin die Überschrift";
+
+	const bagel = makeContentDiv();
+	bagel.innerText = "Ich bin ein Bagel *hmmm*";
+
+
+
+	menu.append(header,bagel);
 	return menu;
 }
