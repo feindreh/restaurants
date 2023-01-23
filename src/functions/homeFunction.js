9f766a62b5bae8abe8b9f91e0f57d76fbe0942ed
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-23 05:54:40 +0100

    added actual content

--- src/functions/homeFunction.js
+++ src/functions/homeFunction.js
@@ -6,7 +6,7 @@ export default function homeFunction(){
 	
 	const header = makeContentDiv();
 	header.setAttribute("class","headline");
-	header.innerText = "Ich bin die Überschrift";
+	header.innerText = "The Bagel Shop";
 
 	const content = document.createElement("div");
 	content.setAttribute("class","content");
@@ -15,8 +15,9 @@ export default function homeFunction(){
 
 
 	const introduction = makeContentDiv();
-	introduction.innerText ="Ich bin die Einführung";
-
+	introduction.innerHTML = "<p>There is more to our humble bagel than meets the eye.</p><p>We are proud to partner with the finest local producers of small goods and fresh produce, delivered daily to bring nothing but the best to every holey bite.</p><p>Keep it simple by playing to the traditional fillings, build your own, or order the lot. The Bagel Shop has something on the menu for every schmood.\nSchmearing, toasting, filling and brewing, 7am to 4pm daily.</p>";
+	introduction.style = "display:flex;flex-direction:column;justify-content:center;gap:15px";
+	
 	const workingHours = makeContentDiv();
 	workingHours.innerText = "Ich bin die Öffnungszeit";
 
